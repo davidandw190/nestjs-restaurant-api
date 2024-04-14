a1a4b51150e6e08adb1c6a921c8184059b764d1b
Andrei-David Nan <nandavid190@gmail.com>
2024-04-14 15:58:30 +0300

    v0.2.3 - set up s3 images deletion;

--- src/restaurant/utils/images.utils.ts
+++ src/restaurant/utils/images.utils.ts
@@ -42,4 +42,32 @@ export default class ImagesUtils {
       throw new Error('Failed to upload images: ' + error.message);
     }
   }
+
+  static async delete(images): Promise<boolean> {
+    const s3 = new S3({
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
+      secretAccessKey: process.env.AWS_SECRET_KEY || '',
+    });
+
+    const imagesKeys = images.map((image) => {
+      return { Key: image.Key };
+    });
+
+    const params = {
+      Bucket: process.env.AWS_S3_BUCKET_NAME || '',
+      Delete: {
+        Objects: imagesKeys,
+        Quiet: false,
+      },
+    };
+
+    try {
+      const data = await s3.deleteObjects(params).promise();
+      console.log('Images deleted successfully:', data);
+      return true;
+    } catch (error) {
+      console.error('Error deleting images:', error);
+      throw new Error('Failed to delete images: ' + error.message);
+    }
+  }
 }
