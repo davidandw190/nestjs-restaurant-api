cac94afdb865f035160d56879d5bde90d345e9c2
Andrei-David Nan <nandavid190@gmail.com>
2024-05-04 17:31:22 +0300

    v0.3.14 - add from-current-user decorator, moved all decorators into app module;

---
+++ src/common/decorators/from-current-user.decorator.ts
@@ -0,0 +1,20 @@
+import { ExecutionContext, createParamDecorator } from '@nestjs/common';
+
+import { TokenPayload } from 'src/auth/types/jwt-token.payload';
+
+/**
+ * Custom param decorator to extract data from the current user's token payload.
+ * If no specific data key is provided, returns the entire user payload.
+ *
+ * @param data - Optional key of the user payload to extract.
+ * @param context - ExecutionContext containing information about the current request.
+ * @returns The specified data from the current user's token payload, or the entire
+ * payload if no key is provided.
+ */
+export const FromCurrentUserDecorator = createParamDecorator(
+  (data: keyof TokenPayload | undefined, context: ExecutionContext) => {
+    const request = context.switchToHttp().getRequest();
+    if (!data) return request.user;
+    return request.user[data];
+  },
+);
--- src/common/decorators/public-resource.decorator.ts
+++ src/common/decorators/public-resource.decorator.ts
@@ -1,4 +1,4 @@
-import { IS_PUBLIC_KEY } from '../constants/auth.constants';
+import { IS_PUBLIC_KEY } from '../../auth/constants/auth.constants';
 import { SetMetadata } from '@nestjs/common';
 
 export const PublicResource = () => SetMetadata(IS_PUBLIC_KEY, true);
