8cbf07a504454c525044b5ffb6bf141bf086969f
Andrei-David Nan <nandavid190@gmail.com>
2024-05-04 18:06:38 +0300

    v0.3.15 - bug fixes regarding providers not exported from user module;

--- src/auth/strategies/access-token.strategy.ts
+++ src/auth/strategies/access-token.strategy.ts
@@ -1,20 +1,21 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 
-import { ConfigService } from '@nestjs/config/dist/config.service';
+import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { TokenPayload } from '../types/jwt-token.payload';
-import { UnauthorizedException } from '@nestjs/common';
 
 /**
  * Strategy to handle access token authentication.
  * This strategy extracts the access token from the request header as a bearer token
  * and validates it against the configured secret key and expiration date.
  */
+@Injectable()
 export class AccessTokenStrategy extends PassportStrategy(
   Strategy,
   'access-token',
 ) {
-  constructor(private readonly configService: ConfigService) {
+  constructor(configService: ConfigService) {
     super({
       ignoreExpiration: false,
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
--- src/auth/user/user.module.ts
+++ src/auth/user/user.module.ts
@@ -6,5 +6,6 @@ import { UserService } from './user.service';
 @Module({
   imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
   providers: [UserService],
+  exports: [UserService],
 })
 export class UserModule {}
